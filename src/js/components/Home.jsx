b33ec8ba6e5e8d0efe3dfd0c6b9bdbfb56665da8
Alexisca <150233709+Thealexisca22@users.noreply.github.com>
2025-08-06 22:32:34 +0000

    antes de evaluar

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,35 +2,75 @@ import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import {TodoList} from "./TodoList.jsx"
+import { TodoList } from "./TodoList.jsx"
 //create your first component
 const Home = () => {
 	const [todos, setTodos] = useState([])
-	
-	
-	
-	const getTodos = async () =>{   
-		const url = "https://playground.4geeks.com/todo/users/manteca"
-		const response = await fetch(url)	
-		if (!response.ok ){
+
+
+
+	const url = "https://playground.4geeks.com/todo/"
+
+	const getTodos = async () => {
+		const response = await fetch(url + "users/manteca")
+		if (!response.ok) {
 			console.log("usuario no encontrado")
 
 		}
-		const data = await response.json() 
+		const data = await response.json()
 		setTodos(data.todos)
 	}
-	const createTodos = () =>{console.log()}
-    const deleteTodos = () =>{console.log()}	
-	useEffect (()=>{getTodos()},[]) 
+
+
+	const createTodo = async (label) => {
+		const response = await fetch(url + "todos/manteca", {
+			method: "POST",
+			body: JSON.stringify({
+				label: label,
+				is_done: false,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+
+
+	}
+	const deleteTodo = async (id) => {
+
+		const response = await fetch(url + "todos/" + id, {
+			method: "DELETE"
+		}
+		)
+
+  
+	}
+
+	const updateTodo = async (id,label) => {
+		const response = await fetch(url + "todos/" + id, {
+			method: "PUT",	
+		    body: JSON.stringify({
+				label: label,
+				is_done: false,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}
+	    )
+	}
 	
-	
-	
-	
-	
+	useEffect(() => { getTodos() }, [])
+
+
+
+
+
 	return (
 		<div>
-        <TodoList todos={todos}/>    
-        
+			<TodoList todos={todos} createTodo={createTodo} getTodos={getTodos} deleteTodo={deleteTodo} updateTodo={updateTodo} />
+
 		</div>
 	);
 };
--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
 
-export const TodoList = ({todos}) => {
+export const TodoList = ({ todos, createTodo, getTodos, deleteTodo, updateTodo }) => {
 
-const [input, setInput] = useState("")
+	const [input, setInput] = useState("")
 	const [hover, setHover] = useState(null)
+	const [show, setShow] = useState(false)
 
 	const handleInput = (e) => {
 		setInput(e.target.value)
@@ -11,25 +12,61 @@ const [input, setInput] = useState("")
 	const enviar = (e) => {
 		if (e.key === "Enter") {
 			e.preventDefault()
-			setCompra([...todos, input])
+
+			if (input.trim() === "") {
+				setShow(true)
+				setTimeout(() => {
+					setShow(false)
+					return
+
+				}, 3000)
+			} else
+
+				createTodo(input)
 			setInput("")
+			getTodos()
+			getTodos()
 		}
 	}
-	const deleteItem = (i) => {
-		let newCompra = todos.filter((_, index) => index !== i)
-		setCompra(newCompra)
+
+	const deleteItem = (id) => {
+		deleteTodo(id)
+		getTodos()
+		getTodos()
+		getTodos()
+	}
+
+	const updateItem = (id) => {
+		if (input.trim() === "") {
+			setShow(true)
+			setTimeout(() => {
+				setShow(false)
+				return
+
+			}, 3000)
+		} else
+			updateTodo(id, input)
+		getTodos()
+		getTodos()
+
 	}
 
 	return (
 		<div className="container">
 			<div className="mx-auto row border border-black border-1 justify-content-center" style={{ maxWidth: "800px" }}>
-
-				<h1 className="col-6 text-center">Lista de la todos</h1>
-
+				{show && (
+					<div class="alert alert-danger" role="alert">
+						Introduce algun valor
+					</div>
+				)}
+				<h1 className="col-6 text-center">Lista de la compra</h1>
+				<p className="text-center">Para editar un producto sigue estos pasos:
+					<br /><strong>1:</strong> Introduce el nuevo valor/producto en la caja de texto de abajo,
+					<br /><strong>2:</strong> Apreta el lapiz al lado de la papelera</p>
 
 				<input
 					className="form-control"
-					placeholder="Producto para todosr"
+					placeholder="Introducir producto"
 					type="text"
 					style={{ width: "680px", }}
 					onKeyUp={(e) => enviar(e)}
@@ -41,18 +78,22 @@ const [input, setInput] = useState("")
 					{todos.map((element, index) => (
 						<div>
 							<li key={element.id}
-								className="d-flex justify-content-between border"
-								onMouseEnter={()=>setHover(index)}
-								onMouseLeave={()=>setHover(null)}
+								className="d-flex border justify-content-between "
+								onMouseEnter={() => setHover(index)}
+								onMouseLeave={() => setHover(null)}
 
 							>
-								<span>{element}</span>
-								
-								<span 
-								onClick={()=>deleteItem(index)}
-								className={hover === index ? "" : "d-none"}
-								>
-<button>hola</button>								</span>
+								<span>{element.label}</span>
+								<div>
+									<span className={hover === index ? "" : "d-none"}
+										onClick={() => updateItem(element.id)}
+									> <i class="fa-solid fa-pen"></i> </span>
+									<span
+										onClick={() => deleteItem(element.id)}
+										className={hover === index ? "" : "d-none"}
+									>
+										<i class="fa-solid fa-trash"></i>	</span>
+								</div>
 							</li>
 						</div>
 					))}
